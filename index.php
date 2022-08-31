33de8e544a0ba97183b37da74dca9927c7a58664
“André <andre_ranulfo@hotmail.com>
2022-08-31 16:06:48 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require 'parashat.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+$myTempDate = DateTime::createFromFormat('Y-m-d', $parashat_date);
+$formatParashatDate = $myTempDate->format('m/d/Y');
+?>
+  <h2><?php echo $formatParashatDate;  ?></h2>
+  <h1>This week's parashat: <?php echo $parashat_title; ?></h1>
+  <h3>Hebrew name: <?php echo $parashat_hebrew; ?></h3>
+  <p>Torah book: <?php echo $parashat_torah; ?></p>
+  <p><?php echo $haftorah; ?></p>
+
+
+  
+</body>
+</html>
---
+++ parashat.php
@@ -0,0 +1,118 @@
+<?php
+//Fetch data from API
+function kaluach()
+{
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=x&ss=on&mf=on&c=on&geo=geoname&geonameid=3448439&m=50&s=on',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+  ));
+
+  $response = curl_exec($curl);
+
+  return json_decode($response, JSON_PRETTY_PRINT);
+  curl_close($curl);
+}
+
+// Filter data from API
+
+//php now
+$now = new DateTime();
+//timezone BST (Brazilian Standard Time)
+$now->setTimezone(new DateTimeZone('America/Sao_Paulo'));
+// date forma (Y-m-d)
+$date = $now->format('Y-m-d');
+
+// Store data in array
+$arr = kaluach();
+
+// Get raw data and store in array
+for ($i = 0; $i < count($arr['items']); $i++) {
+  //Vars
+  $parashat_title;
+  $parashat_date;
+  $parashat_hebrew;
+  $parashat_english;
+  $parashat_torah;
+  $previews_parashat;
+  $next_parashat;
+  //Get this week's parashat
+  if ($arr['items'][$i]['category'] == "parashat") {
+    //Data to be evaluated
+    $parashat_end = $arr['items'][$i]['date'];
+    $parashat_start = date('Y-m-d', strtotime($parashat_end . ' -6 days'));
+    $parashat_title = $arr['items'][$i]['title'];
+
+    //Filter week's parashat by Date
+    if ($date >= $parashat_start && $date <= $parashat_end) {
+      //Store data vars
+      $parashat_title = $arr['items'][$i]['title'];
+      $parashat_title = substr($parashat_title, strpos($parashat_title, ' ') + 1);
+      $parashat_date = $arr['items'][$i]['date'];
+      $parashat_hebrew = $arr['items'][$i]['hebrew'];
+      $parashat_torah = $arr['items'][$i]['leyning']['torah'];
+      $torah_trad = explode(" ", $parashat_torah);
+
+      // array with variables above
+      $parashat_array = array(
+        'parashat_title' => $parashat_title,
+        'parashat_date' => $parashat_date,
+        'parashat_hebrew' => $parashat_hebrew,
+        'parashat_torah' => "", // Next SWITCH
+        'haftorah' => "", // See IF below SWITCH
+      );
+      switch ($torah_trad[0]) {
+        case 'Genesis':
+          $parashat_torah = str_replace('Genesis', 'Bereshit/Gênesis', $torah_trad[0]);
+          $parashat_array['parashat_torah'] = $parashat_torah;
+
+          break;
+        case 'Exodus':
+          $parashat_torah = str_replace('Exodus', 'Shemot/Êxodo', $torah_trad[0]);
+          $parashat_array['parashat_torah'] = $parashat_torah;
+          break;
+
+        case 'Leviticus':
+          $parashat_torah = str_replace('Leviticus', 'Vayikra/Liviticos', $torah_trad[0]);
+          $parashat_array['parashat_torah'] = $parashat_torah;
+          break;
+
+        case 'Numbers':
+          $parashat_torah = str_replace('Numbers', 'Bamidbar/Números', $torah_trad[0]);
+          $parashat_array['parashat_torah'] = $parashat_torah;
+          break;
+
+        case 'Deuteronomy':
+          $parashat_torah = str_replace('Deuteronomy', 'Devarim/Deuteronômio', $torah_trad[0]);
+          $parashat_array['parashat_torah'] = $parashat_torah;
+          break;
+
+        default:
+
+          break;
+      }
+      //Haftarah
+      $haftorah = "";
+      $haftorahAshk = "";
+      $parashat_haftorah_seph = (isset($arr['items'][$i]['leyning']['haftarah_sephardic'])) ? $arr['items'][$i]['leyning']['haftarah_sephardic'] : "";
+      $parashat_haftorah_ashk = $arr['items'][$i]['leyning']['haftarah'];
+
+      if ($parashat_haftorah_seph != "") {
+        $haftorah = "Haftarah: " . $parashat_haftorah_seph . "Ashkenazi: " . $parashat_haftorah_ashk;
+        $parashat_array['haftorah'] = $haftorah;
+      } else {
+        $haftorah = $parashat_haftorah_ashk;
+        $parashat_array['haftorah'] = $haftorah;
+      }
+    }
+  }
+}
+?>
